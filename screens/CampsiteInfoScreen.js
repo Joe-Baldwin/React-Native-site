33a6277cc82b6ea5fc803cea647b786377dea563
Joe Baldwin <124543001+JoeB007@users.noreply.github.com>
2023-07-18 19:34:23 -0500

    animations

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -5,6 +5,7 @@ import RenderCampsite from '../features/campsites/RenderCampsite';
 import { useState } from 'react';
 import { Rating, Input } from 'react-native-elements'; 
 import { postComment } from '../features/comments/commentsSlice';
+import * as Animatable from 'react-native-animatable'
 
 
 const CampsiteInfoScreen = ({ route }) => {
@@ -53,7 +54,7 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
-        <>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
             <FlatList
                 data={comments.commentsArray.filter(
                     (comment) => comment.campsiteId === campsite.id
@@ -129,7 +130,7 @@ const CampsiteInfoScreen = ({ route }) => {
                     </View>
                 </View>
             </Modal>
-        </>
+        </Animatable.View>
     );
 };
 
